15befe2f4c6ad066898987b86b9f04370bac4520
mndowne <mndowne@gmail.com>
2016-11-05 19:12:39 -0500

    getting set up for heroku

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 
-app.set('port', process.env);
+app.set('port', (process.env.PORT || 3000));
 
-app.listen(app.get('port'));
+
+app.listen(app.get('port'), function () {console.log('server is up on port 3000 or the process.env.PORT');});
